b542fd1b50ae6d61b6e3799f7c3eb31874892eab
田能 <neng.tian@patpat.com>
2024-04-04 12:38:22 +0800

    feat(front): docker

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jingdong2-vue
+# jingdong-vue
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <script setup>
 </script>
-
 <template>
   <header>
     <div>
-      hello world
     </div>
   </header>
+
+  <body>
+    <div class="docker">
+      <div class="docker__item">首页</div>
+      <div class="docker__item">订单</div>
+      <div class="docker__item">购物车</div>
+      <div class="docker__item">我的</div>
+    </div>
+
+  </body>
 </template>
 
-<style></style>
+<style type="scss">
+.docker {
+  display: flex;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: .49rem;
+  width: 100%;
+  padding: 0 .18rem;
+  box-sizing: border-box;
+  /*调整计算宽度的模型*/
+  background-color: green;
+}
+
+.docker__item {
+  flex: 1;
+  font-size: .12rem;
+  text-align: center;
+}
+</style>
